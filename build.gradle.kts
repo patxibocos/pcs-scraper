ce615fcba93779dbf8d24d4b37a14d419ea7bbf0
Patxi Bocos <patxi952@gmail.com>
2021-06-22 13:32:38 +0200

    Jar archiving

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,16 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.5.10"
     kotlin("plugin.serialization") version "1.5.10"
+    application
 }
 
 group = "io.github.patxibocos"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("MainKt")
+    applicationName = rootProject.name
+}
 
 repositories {
     mavenCentral()
@@ -16,6 +22,7 @@ dependencies {
     testImplementation(kotlin("test"))
 
     implementation("it.skrape:skrapeit:1.1.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-cli-jvm:0.3.2")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
 }
 
@@ -25,4 +32,13 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<Jar> {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest {
+        attributes["Main-Class"] = application.mainClass
+        archiveFileName.set("${application.applicationName}.jar")
+    }
+    from(configurations.compileClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
 }
