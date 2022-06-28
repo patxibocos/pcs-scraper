488bb321c97fa9bd8996dd65eec236c6a1782254
Patxi Bocos <patxi952@gmail.com>
2022-06-28 22:39:27 +0200

    Http client timeouts and infinite retries

--- src/main/kotlin/io/github/patxibocos/pcsscraper/document/DocFetcher.kt
+++ src/main/kotlin/io/github/patxibocos/pcsscraper/document/DocFetcher.kt
@@ -3,6 +3,7 @@ package io.github.patxibocos.pcsscraper.document
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.request.get
 import io.ktor.client.statement.HttpResponse
 import it.skrape.core.htmlDocument
@@ -18,25 +19,21 @@ class DocFetcher(
     private val skipCache: Boolean,
 ) {
     companion object {
-        private const val MAX_ATTEMPTS = 10
         private const val RETRY_DELAY = 1_000L
     }
 
-    private val client = HttpClient(CIO)
-
-    private suspend fun retry(docURL: URL, attempts: Int, init: (Doc.() -> Unit)?): Doc {
-        delay(RETRY_DELAY)
-        return getDoc(docURL, attempts - 1, init)
+    private val client = HttpClient(CIO) {
+        install(HttpTimeout) {
+            this.requestTimeoutMillis = 10_000
+            this.connectTimeoutMillis = 10_000
+            this.socketTimeoutMillis = 10_000
+        }
     }
 
-    suspend fun getDoc(
+    private suspend fun fetchDoc(
         docURL: URL,
-        attempts: Int = MAX_ATTEMPTS,
-        init: (Doc.() -> Unit)? = null,
-    ): Doc = coroutineScope {
-        if (attempts == 0) {
-            throw IllegalStateException("no more attempts remaining to fetch $docURL")
-        }
+        init: (Doc.() -> Unit)?,
+    ): Doc? {
         val path = docURL.file.dropWhile { it == '/' }
         val splits = path.split("/")
         val normalizeParentPath = if (splits.size > 1) splits.dropLast(1).joinToString(separator = "/", postfix = "/") {
@@ -45,7 +42,7 @@ class DocFetcher(
         val cacheKey = "$normalizeParentPath${splits.last()}"
         val cacheContent = if (!skipCache && cache != null) cache.get(cacheKey) else null
         if (cacheContent != null) {
-            return@coroutineScope htmlDocument(cacheContent) {
+            return htmlDocument(cacheContent) {
                 init?.invoke(this)
                 this
             }
@@ -57,7 +54,7 @@ class DocFetcher(
             }
         } catch (t: Throwable) {
             println("Exception occurred when fetching: ${t.message}")
-            return@coroutineScope retry(docURL, attempts, init)
+            return null
         }
         val byteArrayBody: ByteArray = httpResponse.body()
         val remoteContent = String(byteArrayBody)
@@ -67,9 +64,23 @@ class DocFetcher(
         }
         if (fetchedDoc.isEmpty()) {
             println("Empty document detected, fetching again")
-            return@coroutineScope retry(docURL, attempts, init)
+            return null
         }
         cache?.put(cacheKey to remoteContent)
+        return fetchedDoc
+    }
+
+    suspend fun getDoc(
+        docURL: URL,
+        init: (Doc.() -> Unit)? = null,
+    ): Doc = coroutineScope {
+        var fetchedDoc: Doc? = null
+        while (fetchedDoc == null) {
+            fetchedDoc = fetchDoc(docURL, init)
+            if (fetchedDoc == null) {
+                delay(RETRY_DELAY)
+            }
+        }
         return@coroutineScope fetchedDoc
     }
 }
