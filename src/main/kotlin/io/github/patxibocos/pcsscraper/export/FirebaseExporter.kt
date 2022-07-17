bf7f9c2d361837072bf1836fe2764f7121c25ca2
Patxi Bocos <patxi952@gmail.com>
2022-07-17 22:36:20 +0200

    Workaround to capture Firebase Config version mismatch

--- src/main/kotlin/io/github/patxibocos/pcsscraper/export/FirebaseExporter.kt
+++ src/main/kotlin/io/github/patxibocos/pcsscraper/export/FirebaseExporter.kt
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigException
 import com.google.firebase.remoteconfig.Parameter
 import com.google.firebase.remoteconfig.ParameterValue
 import com.google.firebase.remoteconfig.Template
@@ -12,11 +13,13 @@ import io.github.patxibocos.pcsscraper.entity.Race
 import io.github.patxibocos.pcsscraper.entity.Rider
 import io.github.patxibocos.pcsscraper.entity.Team
 import io.github.patxibocos.pcsscraper.export.protobuf.buildCyclingDataProtobuf
+import mu.KotlinLogging
+import org.slf4j.Logger
 import java.io.ByteArrayOutputStream
 import java.util.Base64
 import java.util.zip.GZIPOutputStream
 
-internal class FirebaseExporter : Exporter {
+internal class FirebaseExporter(private val logger: Logger = KotlinLogging.logger {}) : Exporter {
 
     override suspend fun export(teams: List<Team>, riders: List<Rider>, races: List<Race>) {
         val options = FirebaseOptions.builder()
@@ -28,9 +31,23 @@ internal class FirebaseExporter : Exporter {
 
         val cyclingDataGzipBase64 = gzipThenBase64(cyclingData)
 
-        val template: Template = FirebaseRemoteConfig.getInstance().templateAsync.get()
-        template.parameters["cycling_data"] = Parameter().setDefaultValue(ParameterValue.of(cyclingDataGzipBase64))
-        FirebaseRemoteConfig.getInstance().publishTemplate(template)
+        val firebaseRemoteConfig = FirebaseRemoteConfig.getInstance()
+        val templateBeforeUpdate: Template = firebaseRemoteConfig.template
+        templateBeforeUpdate.parameters["cycling_data"] =
+            Parameter().setDefaultValue(ParameterValue.of(cyclingDataGzipBase64))
+        try {
+            firebaseRemoteConfig.publishTemplate(templateBeforeUpdate)
+        } catch (e: FirebaseRemoteConfigException) {
+            logger.error("Firebase threw an exception", e)
+            val templateAfterUpdate = firebaseRemoteConfig.template
+            val previousVersionNumber = templateBeforeUpdate.version.versionNumber
+            val currentVersionNumber = templateAfterUpdate.version.versionNumber
+            if (templateBeforeUpdate.version == templateAfterUpdate.version) {
+                logger.error("Version hasn't increased")
+                throw e
+            }
+            logger.info("Previous version was $previousVersionNumber and the current is $currentVersionNumber")
+        }
     }
 
     private fun gzipThenBase64(message: MessageLite): String =
