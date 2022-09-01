83c100b149ae47c0c55791126343d01d91d08ff1
Patxi Bocos <patxi952@gmail.com>
2022-09-01 23:08:00 +0200

    Publishing a message on FCM topic when stage results available

--- diff-publisher/src/main/kotlin/io/github/patxibocos/diffpublisher/Main.kt
+++ diff-publisher/src/main/kotlin/io/github/patxibocos/diffpublisher/Main.kt
@@ -3,10 +3,11 @@ package io.github.patxibocos.diffpublisher
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.Message
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.ParameterValue
 import io.github.patxibocos.pcsscraper.protobuf.CyclingDataOuterClass
-import io.github.patxibocos.pcsscraper.protobuf.RaceOuterClass.ParticipantResult
 import mu.KotlinLogging
 import org.slf4j.Logger
 import java.io.ByteArrayInputStream
@@ -53,12 +54,16 @@ private fun checkNewStagesWithResults(
     val logger: Logger = KotlinLogging.logger {}
     val lastStageResults = lastData.racesList.flatMap { it.stagesList }.associate { it.id to it.resultList }
     val previousStageResults = previousData.racesList.flatMap { it.stagesList }.associate { it.id to it.resultList }
-    val newStageResults: MutableList<Pair<String, List<ParticipantResult>>> = mutableListOf()
     lastStageResults.forEach { (stageId, stageResults) ->
         val previousResults = previousStageResults[stageId]
         if (previousResults != null && previousResults.isEmpty() && stageResults.isNotEmpty()) {
             logger.info("Results available for $stageId")
-            newStageResults.add(stageId to stageResults)
+            val message = Message.builder()
+                .setTopic("stage-results")
+                .putData("stage-id", stageId)
+                .build()
+            val firebaseMessaging = FirebaseMessaging.getInstance()
+            firebaseMessaging.send(message)
         }
     }
 }
